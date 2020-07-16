9d7c4b2751bb3cac3155f901f021c290b04019e6
Wafi Azmi Hartono <wafiazmi.ahartono@gmail.com>
2020-07-17 01:24:15 +0700

    Menambahkan file test

---
+++ test/daftar-mata-kuliah.js
@@ -0,0 +1,55 @@
+const daftarMataKuliah = [
+    {
+        nama: 'Mata Kuliah 1',
+        kode: '00001',
+        kelas: [
+            {
+                kode: 'A',
+                jadwal: [
+                    {
+                        hari: 'Senin',
+                        jamMulai: '06.00',
+                        jamBerakhir: '08.00'
+                    },
+                    {
+                        hari: 'Selasa',
+                        jamMulai: '10.00',
+                        jamBerakhir: '12.00'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nama: 'Mata Kuliah 2',
+        kode: '00002',
+        kelas: [
+            {
+                kode: 'A',
+                jadwal: [
+                    {
+                        hari: 'Senin',
+                        jamMulai: '07.00',
+                        jamBerakhir: '09.00'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nama: 'Mata Kuliah 3',
+        kode: '00003',
+        kelas: [
+            {
+                kode: 'A',
+                jadwal: [
+                    {
+                        hari: 'Selasa',
+                        jamMulai: '09.00',
+                        jamBerakhir: '11.00'
+                    }
+                ]
+            }
+        ]
+    }
+];
